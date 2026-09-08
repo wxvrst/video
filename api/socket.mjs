6dd5ed349dc614487b5f82e79a16a7ea2944dae3
Сафонтьев Иван <safontev@bk.ru>
2026-09-09 00:17:01 +0500

    fix

--- api/socket.mjs
+++ api/socket.mjs
@@ -3,23 +3,21 @@ import { Server } from 'socket.io';
 import { version, validate } from 'uuid';
 
 const ACTIONS = {
-	JOIN: "join",
-	LEAVE: "leave",
-	SHARE_ROOMS: "share-rooms",
-	ADD_PEER: "add-peer",
-	REMOVE_PEER: "remove-peer",
-	RELAY_SDP: "relay-sdp",
-	RELAY_ICE: "relay-ice",
-	ICE_CANDIDATE: "ice-candidate",
-	SESSION_DESCRIPTION: "session-description",
+  JOIN: "join",
+  LEAVE: "leave",
+  SHARE_ROOMS: "share-rooms",
+  ADD_PEER: "add-peer",
+  REMOVE_PEER: "remove-peer",
+  RELAY_SDP: "relay-sdp",
+  RELAY_ICE: "relay-ice",
+  ICE_CANDIDATE: "ice-candidate",
+  SESSION_DESCRIPTION: "session-description",
 };
 
 const server = createServer();
 const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
+  cors: { origin: '*', methods: ['GET', 'POST'] },
+  path: '/api/socket/socket.io',
 });
 
 function getClientRooms() {
@@ -91,6 +89,5 @@ io.on('connection', (socket) => {
     });
   });
 });
-
 
 export default server;
